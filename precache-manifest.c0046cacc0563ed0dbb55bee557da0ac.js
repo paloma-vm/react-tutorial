9a29413da390c35939b9ca72187135db4b5b66d6
Paloma Valdez-Marsh <palomavaldez-marsh@Palomas-MacBook-Air.local>
2023-02-19 23:31:42 -0700

    Updates

---
+++ precache-manifest.c0046cacc0563ed0dbb55bee557da0ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8848c2f352a68c3c6085",
+    "url": "/static/css/main.fbf3e6e7.chunk.css"
+  },
+  {
+    "revision": "8848c2f352a68c3c6085",
+    "url": "/static/js/main.8848c2f3.chunk.js"
+  },
+  {
+    "revision": "e8e3b76c5bf759905946",
+    "url": "/static/js/1.e8e3b76c.chunk.js"
+  },
+  {
+    "revision": "ae974b997d82985e27f3",
+    "url": "/static/js/2.ae974b99.chunk.js"
+  },
+  {
+    "revision": "316baba7ca5f9e773de3",
+    "url": "/static/js/runtime~main.316baba7.js"
+  },
+  {
+    "revision": "1eeb4c12578504b9b8e139baa4016d3a",
+    "url": "/index.html"
+  }
+];
